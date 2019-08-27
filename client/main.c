9c13abc60b483c106e69d3e2047f94d42c7bdabd
wmjtxt <972213032@qq.com>
2019-08-27 17:30:09 +0800

    client

--- client/main.c
+++ client/main.c
@@ -10,7 +10,7 @@ int main(int argc,char** argv)
 	sfd=socket(AF_INET,SOCK_STREAM,0);
 	if(-1==sfd){
 		perror("socket");
-		return -1
+		return -1;
 	}
 	struct sockaddr_in ser;
 	memset(&ser,0,sizeof(ser));
